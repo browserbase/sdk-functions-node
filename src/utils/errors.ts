e608efea8a7dfe0b081e900d7e3d072347482076
ajmcquilkin <46639306+ajmcquilkin@users.noreply.github.com>
2025-09-11 10:13:09 -0700

    Add helpers

---
+++ src/utils/errors.ts
@@ -0,0 +1,23 @@
+export class FunctionNotFoundInRegistryError extends Error {
+  constructor(message: string) {
+    super(message);
+  }
+}
+
+export class GetNextInvocationError extends Error {
+  constructor(message: string) {
+    super(message);
+  }
+}
+
+export class PostResultError extends Error {
+  constructor(message: string) {
+    super(message);
+  }
+}
+
+export class PostErrorError extends Error {
+  constructor(message: string) {
+    super(message);
+  }
+}
