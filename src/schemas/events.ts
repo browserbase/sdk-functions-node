4363c98a9d03b1af5b4a4f5524585912f79d3cae
ajmcquilkin <46639306+ajmcquilkin@users.noreply.github.com>
2025-09-29 17:20:21 -0700

    Cleanup with `looseObject`

--- src/schemas/events.ts
+++ src/schemas/events.ts
@@ -2,24 +2,24 @@ import z from "zod";
 import { FunctionInvocationContext } from "./invocation.js";
 
 export const RuntimeEventPayload = z.object({
-  functionName: z.string(),
-  params: z.any(),
+  functionName: z.string().min(1),
+  params: z.looseObject({}), // Allow passthrough of unknown (all) keys
   context: FunctionInvocationContext,
 });
 
 export type RuntimeEventPayload = z.infer<typeof RuntimeEventPayload>;
 
 export const RuntimeEvent = z.object({
-  requestId: z.string(),
+  requestId: z.string().min(1),
   event: RuntimeEventPayload,
 });
 
 export type RuntimeEvent = z.infer<typeof RuntimeEvent>;
 
 export const RuntimeError = z.object({
-  errorMessage: z.string(),
-  errorType: z.string(),
-  stackTrace: z.array(z.string()),
+  errorMessage: z.string().min(1),
+  errorType: z.string().min(1),
+  stackTrace: z.array(z.string().min(1)),
 });
 
 export type RuntimeError = z.infer<typeof RuntimeError>;
