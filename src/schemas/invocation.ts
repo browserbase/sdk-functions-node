e7648a5feab5bb2b61d5f2c028a29cac58673225
Adam McQuilkin <46639306+ajmcquilkin@users.noreply.github.com>
2026-01-13 14:18:21 -0800

    Don't strip additional config

--- src/schemas/invocation.ts
+++ src/schemas/invocation.ts
@@ -1,17 +1,21 @@
 import z from "zod";
 
-export const FunctionInvocationContextSessionDetails = z.object({
-  id: z.string(),
-  connectUrl: z.string(),
-});
+export const FunctionInvocationContextSessionDetails = z
+  .object({
+    id: z.string(),
+    connectUrl: z.string(),
+  })
+  .loose(); // Allow in additional fields without needing to update the SDK version
 
 export type FunctionInvocationContextSessionDetails = z.infer<
   typeof FunctionInvocationContextSessionDetails
 >;
 
-export const FunctionInvocationContext = z.object({
-  session: FunctionInvocationContextSessionDetails,
-});
+export const FunctionInvocationContext = z
+  .object({
+    session: FunctionInvocationContextSessionDetails,
+  })
+  .loose(); // Allow in additional fields without needing to update the SDK version
 
 export type FunctionInvocationContext = z.infer<
   typeof FunctionInvocationContext
