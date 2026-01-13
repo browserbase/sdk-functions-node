f622d1ba1f66e3cdf4386de4af3e290f20547dbb
Adam McQuilkin <46639306+ajmcquilkin@users.noreply.github.com>
2026-01-13 14:25:54 -0800

    Use v4 `looseObject`

--- src/schemas/invocation.ts
+++ src/schemas/invocation.ts
@@ -1,21 +1,19 @@
 import z from "zod";
 
-export const FunctionInvocationContextSessionDetails = z
-  .object({
-    id: z.string(),
-    connectUrl: z.string(),
-  })
-  .loose(); // Allow in additional fields without needing to update the SDK version
+// Allow in additional fields without needing to update the SDK version
+export const FunctionInvocationContextSessionDetails = z.looseObject({
+  id: z.string(),
+  connectUrl: z.string(),
+});
 
 export type FunctionInvocationContextSessionDetails = z.infer<
   typeof FunctionInvocationContextSessionDetails
 >;
 
-export const FunctionInvocationContext = z
-  .object({
-    session: FunctionInvocationContextSessionDetails,
-  })
-  .loose(); // Allow in additional fields without needing to update the SDK version
+// Allow in additional fields without needing to update the SDK version
+export const FunctionInvocationContext = z.looseObject({
+  session: FunctionInvocationContextSessionDetails,
+});
 
 export type FunctionInvocationContext = z.infer<
   typeof FunctionInvocationContext
